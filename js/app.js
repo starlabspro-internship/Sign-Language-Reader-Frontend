a0490419355acaa5c936375bc613cd83498be7ec
AtdheAb <120095651+AtdheAb@users.noreply.github.com>
2024-10-31 16:29:14 +0100

    Updated fronted components and added a slider

--- js/app.js
+++ js/app.js
@@ -27,6 +27,44 @@ document.querySelectorAll('.read-more').forEach(button => {
     });
 });
 
+//slider functionality
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function() {
+    active = (active + 1 > lengthItems) ? 0 : active + 1;
+    reloadSlider();
+}
+
+prev.onclick = function() {
+    active = (active - 1 < 0) ? lengthItems : active - 1;
+    reloadSlider();
+}
+
+let refreshSlider = setInterval(() => { next.click() }, 5000);
+
+function reloadSlider() {
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    document.querySelector('.slider .dots li.active').classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(() => { next.click() }, 5000);
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', function() {
+        active = key;
+        reloadSlider();
+    });
+});
 
 
 
