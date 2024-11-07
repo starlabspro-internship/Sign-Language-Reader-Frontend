4ec40faec70a321345cea3a2ef630b4a59e4c592
gentonitaa <gf61372@ubt-uni.net>
2024-11-07 18:33:09 +0100

    literaturaaa

---
+++ js/search.js
@@ -0,0 +1,13 @@
+function searchTopics() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const boxes = document.querySelectorAll('.box');
+
+    boxes.forEach(box => {
+        const topic = box.getAttribute('data-topic').toLowerCase(); // Get the data-topic attribute value
+        if (topic.startsWith(searchInput)) {
+            box.style.display = 'block'; // Show matching box
+        } else {
+            box.style.display = 'none'; // Hide non-matching box
+        }
+    });
+}
