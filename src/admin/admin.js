e1b8b37d01c52a4cc5753a7c40f5673258a61118
Enes Gj <enesgjurgjiali04@gmail.com>
2024-12-10 22:24:24 +0100

    Fix the admin-logout function

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -3,6 +3,7 @@ import "../profile/profileManager.js";
 import { sidebar } from "./adminFunctions/adminSidebar.js";
 import { isAdmin } from "./adminFunctions/checkIfAdmin.js";
 
+
 (async () => {
   const userIsAdmin = await isAdmin();
 })();
@@ -11,5 +12,42 @@ document.addEventListener("DOMContentLoaded", async () => {
   const container = document.querySelector(".container");
   if (container) {
     container.insertAdjacentHTML("afterbegin", sidebar);
+  }
+
+  
+  const logoutModal = document.getElementById("logoutModal");
+  const closeButton = document.querySelector(".close-button");
+  const confirmLogoutButton = document.getElementById("confirmLogoutButton");
+  const cancelLogoutButton = document.getElementById("cancelLogoutButton");
+  const logoutButton = document.getElementById("logoutButton");
+  if (logoutButton) {
+    logoutButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      logoutModal.style.display = "flex";
+    });
+  } else {
+    console.error("Logout button not found in the DOM.");
+  }
+  if (closeButton) {
+    closeButton.addEventListener("click", () => {
+      logoutModal.style.display = "none";
+    });
+  } else {
+    console.error("Close button not found in the DOM.");
+  }
+  if (confirmLogoutButton) {
+    confirmLogoutButton.addEventListener("click", async () => {
+      logoutModal.style.display = "none";
+      await handleLogout();
+    });
+  } else {
+    console.error("Confirm logout button not found in the DOM.");
+  }
+  if (cancelLogoutButton) {
+    cancelLogoutButton.addEventListener("click", () => {
+      logoutModal.style.display = "none";
+    });
+  } else {
+    console.error("Cancel logout button not found in the DOM.");
   }
 });
